c2ccbcdc4a41caa07199c1e5982c53ce399ba5b5
Araya <araya.anthichai@gmail.com>
2025-11-30 23:37:24 +0800

    Add nltk_utils.py

---
+++ nltk_utils.py
@@ -0,0 +1,11 @@
+import nltk
+
+def tokenization(sentence):
+    return nltk.word_tokenize(sentence)
+    
+def stem(word):
+    pass
+    
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
