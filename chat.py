ba0e26b8415106cb21ebb0de6e9bb04bd55dcb70
nyklle <kellynnjoycelee@gmail.com>
2025-12-02 17:24:18 +0800

    today's commit

--- chat.py
+++ chat.py
@@ -41,7 +41,7 @@ while True:
 
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
-    print(tag)
+    
     if prob.item() > 0.75:
         for intent in intents["intents"]:
             if tag == intent["tag"]:
