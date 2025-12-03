85beb5892754077eb79c4ff28279346e84124667
Araya <araya.anthichai@gmail.com>
2025-12-03 23:57:36 +0800

    Trying to prompt engineer this thing

--- gemini.py
+++ gemini.py
@@ -2,28 +2,21 @@ bot_name = "Hal"
 print("You are now speaking to Hal, our health pal!")
 print(f"{bot_name}: Hello! I'm {bot_name}, your health pal, how may I help you today?")
 
-while True:
-    user_input: str = input("You: ").lower()
-
-    if user_input in ["hi", "hello"]:
-        print(f"{bot_name}: Hello! How can I help?")
-
-    elif user_input in ["bye", "see you", "goodbye"]:
-        print(f"{bot_name}: Have a great day!")
-
-    else:
-        break
-
+import os
 from google import genai
 
-client = genai.Client(api_key=)
+GEMINI_API_KEY = "AIzaSyDwMgiOf2WDzDoVNLkeP7dGDwHY8S5iKrc"
+client = genai.Client(api_key=GEMINI_API_KEY)
 
+prompt = f"""
+    You are a helpful nutrition assistant. You must only use information from reliable, credible and verifiable sources, such as USDA FoodData Central database, Australia Food Composition Database or reputable nutrition journals and articles. 
+    Do not use unrealiable websites, forums or general internet searches. Cite your sources. If there are no reliable information to be found, say "I'm sorry but I am unable to find any reliable nutrition information for that." 
+    """
 while True:
     user_input = input("You: ")
     if user_input.lower() == "exit":
         break
     response = client.models.generate_content(
-        model="gemini-2.0-flash",
-        contents=user_input
+        model="gemini-2.0-flash", contents=user_input
     )
     print(f"\nHaL: {response.text}")
